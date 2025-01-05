1e60273f77d82b673c4e310191a39daaf9dc21dd
Andreas Korb <andreas.d.korb@gmail.com>
2025-01-05 22:59:35 +0100

    Allow specifying date of files which should be modified

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ def change_file_modification_date(file: str, date: datetime):
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 main.py path")
+    if len(sys.argv) not in [2, 3]:
+        print("Usage: python3 main.py path [date]")
+        print()
+        print("  If a date is specified, only files last modified on this date are corrected.")
+        print("  Date format: 2024-08-26")
         sys.exit(1)
+
+    try:
+        selected_modification_date_filter = datetime.fromisoformat(sys.argv[2])
+    except IndexError:
+        selected_modification_date_filter = None
 
     answer = input("This will change the modifications date of the files in the given path.\nYou might want to make a backup first. Continue? [y/N] ")
     if answer.lower() not in ['y', 'yes']:
@@ -20,6 +28,16 @@ def main():
     dirname = sys.argv[1]
     files = os.scandir(path=dirname)
     for file in files:
+        if not file.is_file():
+            continue
+        if selected_modification_date_filter:
+            modi_time_timestamp = os.stat(file).st_mtime
+            modi_time = datetime.fromtimestamp(modi_time_timestamp)
+            # If the modification date of the currently iterated file
+            # is not what the user specified, just skip it without touching it.
+            if modi_time.date() != selected_modification_date_filter.date():
+                continue
+
         parser_name, date = FileNameToDateParser.try_parse_with_all(file.name)
         if date is not None:
             change_file_modification_date(file.path, date)
